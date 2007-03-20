db6088da22854dd57deb1936547453b68752be2f
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-03-20 20:02:27 +0000

    Master_Sequences updates since December 2006

--- Master_Sequences/trunk/DBDefinition_Master_Sequences.sql
+++ Master_Sequences/trunk/DBDefinition_Master_Sequences.sql
@@ -1,8 +1,8 @@
 /****** Object:  Database [Master_Sequences] ******/
 CREATE DATABASE [Master_Sequences] ON  PRIMARY 
-( NAME = N'Master_Sequences_Data', FILENAME = N'I:\SQLServerData\Master_Sequences_Data.mdf' , SIZE = 78914944KB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)
+( NAME = N'Master_Sequences_Data', FILENAME = N'I:\SQLServerData\Master_Sequences_Data.mdf' , SIZE = 104639424KB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)
  LOG ON 
-( NAME = N'Master_Sequences_Log', FILENAME = N'H:\SQLServerData\Master_Sequences_Log.ldf' , SIZE = 204032KB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)
+( NAME = N'Master_Sequences_Log', FILENAME = N'H:\SQLServerData\Master_Sequences_Log.ldf' , SIZE = 153216KB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)
  COLLATE SQL_Latin1_General_CP1_CI_AS
 GO
 EXEC dbo.sp_dbcmptlevel @dbname=N'Master_Sequences', @new_cmptlevel=90
