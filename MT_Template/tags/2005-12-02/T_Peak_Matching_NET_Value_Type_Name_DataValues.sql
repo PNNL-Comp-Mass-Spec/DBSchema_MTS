f5b1dea47efaa52bad7c3571a9874b9158995263
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-08-01 00:35:27 +0000

    Removed file/folder

--- MT_Template/tags/2005-12-02/T_Peak_Matching_NET_Value_Type_Name_DataValues.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO dbo.T_Peak_Matching_NET_Value_Type_Name
-  VALUES (0, N'GANET - from DB')
-INSERT INTO dbo.T_Peak_Matching_NET_Value_Type_Name
-  VALUES (1, N'PNET - from DB')
-INSERT INTO dbo.T_Peak_Matching_NET_Value_Type_Name
-  VALUES (2, N'Theoretical NET')
-
-go
