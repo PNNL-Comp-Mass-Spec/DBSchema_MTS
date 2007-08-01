6c58cf05f77c68ac7e7cebf58fc00e73084ac2c9
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-08-01 00:49:48 +0000

    Removed file/folder

--- PT_Template/tags/2006-08-02/T_Peptide_Cleavage_State_Name_DataValues.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO dbo.T_Peptide_Cleavage_State_Name
-  VALUES (0, N'non-specific')
-INSERT INTO dbo.T_Peptide_Cleavage_State_Name
-  VALUES (1, N'partial')
-INSERT INTO dbo.T_Peptide_Cleavage_State_Name
-  VALUES (2, N'full')
-
-go
