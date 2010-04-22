08d2fa6695d7e217c2ffcaa24d598f0baf61368a
Matthew Monroe <matthew.monroe@pnnl.gov>
2010-04-22 20:16:09 +0000

    Updates for April 2010

--- Master_Seq_Scratch/UpdateUserPermissions.sql
+++ Master_Seq_Scratch/UpdateUserPermissions.sql
@@ -139,3 +139,7 @@ AS
 	Return 0
 
 GO
+GRANT VIEW DEFINITION ON [dbo].[UpdateUserPermissions] TO [MTS_DB_Dev] AS [dbo]
+GO
+GRANT VIEW DEFINITION ON [dbo].[UpdateUserPermissions] TO [MTS_DB_Lite] AS [dbo]
+GO
