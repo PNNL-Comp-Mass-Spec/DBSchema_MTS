b9def38db38e5c4b8d1cafacce55835bdc3c279c
Matthew Monroe <matthew.monroe@pnnl.gov>
2013-07-24 23:16:43 +0000

    Updates for July 2013

--- dbSNP/SNPAlleleFreq_TGP.sql
+++ dbSNP/SNPAlleleFreq_TGP.sql
@@ -4,11 +4,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[SNPAlleleFreq_TGP](
-	[snp_id] [int] NULL,
+	[snp_id] [int] NOT NULL,
 	[allele_id] [int] NOT NULL,
 	[freq] [float] NOT NULL,
 	[count] [int] NOT NULL,
-	[is_minor_allele] [bit] NOT NULL
+	[is_minor_allele] [bit] NOT NULL,
+ CONSTRAINT [PK_SNPAlleleFreq_TGP] PRIMARY KEY CLUSTERED 
+(
+	[snp_id] ASC,
+	[allele_id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
