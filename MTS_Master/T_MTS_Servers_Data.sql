de7115825cbc242988f7132d79143e8aec6f15d6
alchemistmatt <matthew.monroe@pnnl.gov>
2015-02-26 01:00:38 -0800

    2015-02-26 auto-commit MTS_Master

--- MTS_Master/T_MTS_Servers_Data.sql
+++ MTS_Master/T_MTS_Servers_Data.sql
@@ -2,10 +2,10 @@
 /****** RowCount: 8 ******/
 /****** Columns: Server_ID, Server_Name, Active ******/
 INSERT INTO [T_MTS_Servers] VALUES (1,'Pogo',1)
-INSERT INTO [T_MTS_Servers] VALUES (2,'Albert',1)
+INSERT INTO [T_MTS_Servers] VALUES (2,'Albert',0)
 INSERT INTO [T_MTS_Servers] VALUES (3,'Roadrunner',1)
-INSERT INTO [T_MTS_Servers] VALUES (4,'Porky',1)
-INSERT INTO [T_MTS_Servers] VALUES (5,'Elmer',1)
+INSERT INTO [T_MTS_Servers] VALUES (4,'Porky',0)
+INSERT INTO [T_MTS_Servers] VALUES (5,'Elmer',0)
 INSERT INTO [T_MTS_Servers] VALUES (6,'Daffy',1)
 INSERT INTO [T_MTS_Servers] VALUES (7,'Proteinseqs2',1)
 INSERT INTO [T_MTS_Servers] VALUES (8,'Proteinseqs',1)
