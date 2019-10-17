c3cb748dafbcc7369935c3f319cd4f5af0e520e2
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-16 17:53:16 -0700

    Whitespace updates after switch to SqlManagementObjects v150

--- MT_Template/T_Quantitation_Description.sql
+++ MT_Template/T_Quantitation_Description.sql
@@ -74,7 +74,7 @@ CREATE NONCLUSTERED INDEX [IX_T_Quantitation_Description_Quantitation_State_incl
 (
 	[Quantitation_State] ASC
 )
-INCLUDE ( 	[Quantitation_ID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+INCLUDE([Quantitation_ID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_Quantitation_Description] ADD  CONSTRAINT [DF_T_Quantitation_Description_QuantitationProcessingState]  DEFAULT ((1)) FOR [Quantitation_State]
 GO
