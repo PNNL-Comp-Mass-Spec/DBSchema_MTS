167a78b38b2f23721592480335170c3d1a0477c4
alchemistmatt <matthew.monroe@pnnl.gov>
2019-12-06 12:38:48 -0800

    Update paths for new server

--- MT_Main/T_Folder_Paths_Data.sql
+++ MT_Main/T_Folder_Paths_Data.sql
@@ -1,16 +1,16 @@
 /****** Object:  Table [T_Folder_Paths] ******/
 /****** RowCount: 13 ******/
 /****** Columns: Function, Client_Path, Server_Path ******/
-INSERT INTO [T_Folder_Paths] VALUES ('Database Backup Log Path','I:\SqlServerBackup\','I:\SqlServerBackup\')
+INSERT INTO [T_Folder_Paths] VALUES ('Database Backup Log Path','L:\SQLServerBackup','L:\SQLServerBackup')
 INSERT INTO [T_Folder_Paths] VALUES ('Database Backup Path','\\proto-8\DB_Backups\Pogo_Backup\','\\proto-8\DB_Backups\Pogo_Backup\')
-INSERT INTO [T_Folder_Paths] VALUES ('Default Database Folder','','I:\SqlServerData\')
-INSERT INTO [T_Folder_Paths] VALUES ('Default Database Transaction Log Folder','','H:\SqlServerData\')
-INSERT INTO [T_Folder_Paths] VALUES ('GANET Transfer Root Folder','\\pogo\GA_Net_Xfer\','F:\GA_Net_Xfer')
+INSERT INTO [T_Folder_Paths] VALUES ('Default Database Folder','','J:\SQLServerData')
+INSERT INTO [T_Folder_Paths] VALUES ('Default Database Transaction Log Folder','','L:\SQLServerData')
+INSERT INTO [T_Folder_Paths] VALUES ('GANET Transfer Root Folder','\\pogo\GA_Net_Xfer\','L:\GA_Net_Xfer')
 INSERT INTO [T_Folder_Paths] VALUES ('MultiAlign Parameters','\\sylvester\MTD_Peak_Matching\parameters\MultiAlign\','\\sylvester\MTD_Peak_Matching\parameters\MultiAlign\')
 INSERT INTO [T_Folder_Paths] VALUES ('MultiAlign Results','\\pogo\MTD_Peak_Matching\MultiAlign_results\','L:\MTD_Peak_Matching\MultiAlign_results\')
-INSERT INTO [T_Folder_Paths] VALUES ('MyEMSL Cache Folder','\\pogo\MyEMSL_Cache\','H:\MyEMSL_Cache\')
+INSERT INTO [T_Folder_Paths] VALUES ('MyEMSL Cache Folder','\\pogo\MyEMSL_Cache\','L:\MyEMSL_Cache\')
 INSERT INTO [T_Folder_Paths] VALUES ('Peak Matching Parameters','\\sylvester\MTD_Peak_Matching\parameters\auto_ini_files\','\\sylvester\MTD_Peak_Matching\parameters\auto_ini_files\')
 INSERT INTO [T_Folder_Paths] VALUES ('Peak Matching Results','\\pogo\MTD_Peak_Matching\results\','L:\MTD_Peak_Matching\results\')
 INSERT INTO [T_Folder_Paths] VALUES ('Peak Matching Results Website','http://pogo/pm/results/','http://pogo/pm/results/')
-INSERT INTO [T_Folder_Paths] VALUES ('Peptide Prophet Transfer Root Folder','\\pogo\Peptide_Prophet_Xfer\','F:\Peptide_Prophet_Xfer\')
-INSERT INTO [T_Folder_Paths] VALUES ('Redgate Backup Transfer Folder','','E:\Redgate_Backup_Xfer\')
+INSERT INTO [T_Folder_Paths] VALUES ('Peptide Prophet Transfer Root Folder','\\pogo\Peptide_Prophet_Xfer\','L:\Peptide_Prophet_Xfer\')
+INSERT INTO [T_Folder_Paths] VALUES ('Redgate Backup Transfer Folder','','L:\Redgate_Backup_Xfer\')
