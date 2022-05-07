6c7b9ef2eb7db8a5b24b085cfc4639f3284a539c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-07 01:09:16 -0700

    2022-05-07 auto-commit Master_Sequences

--- Master_Sequences/T_Process_Config_Data.sql
+++ Master_Sequences/T_Process_Config_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Process_Config] ******/
 /****** RowCount: 1 ******/
 SET IDENTITY_INSERT [T_Process_Config] ON
-INSERT INTO [T_Process_Config] (Process_Config_ID, Name, Value) VALUES (100,'Mass_Calculation_Max_Sequence_Count','100000')
+INSERT INTO [T_Process_Config] (Process_Config_ID, Name, [Value]) VALUES (100,'Mass_Calculation_Max_Sequence_Count','100000')
 SET IDENTITY_INSERT [T_Process_Config] OFF
