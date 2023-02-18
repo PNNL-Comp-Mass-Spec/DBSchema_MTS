00af740a1362df38445905dfaae84125ba553c9e
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-18 01:09:47 -0800

    2023-02-18 auto-commit MTS_Master

--- MTS_Master/V_Analysis_Job_to_Peptide_DB_Map.sql
+++ MTS_Master/V_Analysis_Job_to_Peptide_DB_Map.sql
@@ -6,14 +6,14 @@ GO
 
 CREATE VIEW [dbo].[V_Analysis_Job_to_Peptide_DB_Map]
 AS
-SELECT S.Server_Name, 
-       AJPDM.Job, 
-       AJPDM.ResultType, 
-	   ISNULL(D.Peptide_DB_Name, '??') AS DB_Name, 
-	   AJPDM.Last_Affected, 
-	   CONVERT(varchar(12), AJPDM.Process_State) + ': ' + ISNULL(StateName.Name, '??') AS Process_State, 
-	   AJPDM.Server_ID, 
-	   AJPDM.Peptide_DB_ID
+SELECT S.Server_Name,
+       AJPDM.Job,
+       AJPDM.ResultType,
+       ISNULL(D.Peptide_DB_Name, '??') AS DB_Name,
+       AJPDM.Last_Affected,
+       CONVERT(varchar(12), AJPDM.Process_State) + ': ' + ISNULL(StateName.Name, '??') AS Process_State,
+       AJPDM.Server_ID,
+       AJPDM.Peptide_DB_ID
 FROM dbo.T_Analysis_Job_to_Peptide_DB_Map AS AJPDM INNER JOIN
     dbo.T_MTS_Servers AS S ON 
     AJPDM.Server_ID = S.Server_ID LEFT OUTER JOIN
@@ -22,6 +22,5 @@ FROM dbo.T_Analysis_Job_to_Peptide_DB_Map AS AJPDM INNER JOIN
     dbo.T_MTS_Peptide_DBs AS D ON 
     AJPDM.Peptide_DB_ID = D.Peptide_DB_ID AND 
     AJPDM.Server_ID = D.Server_ID
-
 
 GO
